d83842a889ccfc33a233087bb004149a44eb97ad
crossbridge.kato@gmail.com <crossbridge.kato@gmail.com@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2010-12-25 14:36:36 +0000

    AppWidgetの作成(1)

---
+++ trunk/AppWidget1/src/org/jpn/techbooster/sample/appwidget1/AppWidget1.java
@@ -0,0 +1,13 @@
+package org.jpn.techbooster.sample.appwidget1;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class AppWidget1 extends AppWidgetProvider {
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+}
