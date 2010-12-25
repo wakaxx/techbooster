6cd4c12f369697460b7e2e1340db6075e4466634
foresta.profonda <foresta.profonda@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2010-12-25 21:43:00 +0000

    スタイルを使ってレイアウトを定義する

---
+++ trunk/StyleSample/src/org/jpn/techbooster/StyleSample/StyleSample.java
@@ -0,0 +1,15 @@
+package org.jpn.techbooster.StyleSample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StyleSample extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		// レイアウトファイルをご参照下さい
+    }
+}
