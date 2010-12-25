1a933f8311ecaf47c608b7fe4bc88448db7b468b
crossbridge.kato <crossbridge.kato@gmail.com>
2010-12-25 13:46:47 +0000

    画面の向きによってレイアウトを変更する

---
+++ ScreenOrientation/src/org/jpn/techbooster/sample/screenorientation/ScreenOrientationActivity.java
@@ -0,0 +1,13 @@
+package org.jpn.techbooster.sample.screenorientation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScreenOrientationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
