f5fbd67ff9dc5f55e8dd7ac0cb39e7b613fac8e5
k.ikata.xperia <k.ikata.xperia@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2011-04-24 16:30:44 +0000

    TwitterでOAuth認証を行う(2：Twitterの認証ページをブラウザで開く)

---
+++ OauthSample/src/org/jpn/techbooster/sample/OauthSample/CallBackActivity.java
@@ -0,0 +1,15 @@
+package org.jpn.techbooster.sample.OauthSample;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CallBackActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.callback);
+	}
+
+}
