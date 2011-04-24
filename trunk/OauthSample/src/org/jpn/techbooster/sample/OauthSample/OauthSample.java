e1aeb0e36da6ba7840522cb834a8a42e523d4464
k.ikata.xperia <k.ikata.xperia@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2011-04-24 16:30:44 +0000

    TwitterでOAuth認証を行う(2：Twitterの認証ページをブラウザで開く)

---
+++ trunk/OauthSample/src/org/jpn/techbooster/sample/OauthSample/OauthSample.java
@@ -0,0 +1,52 @@
+package org.jpn.techbooster.sample.OauthSample;
+
+import twitter4j.TwitterException;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationContext;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class OauthSample extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button btn = (Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				executeOauth();
+			}
+		});
+    }
+
+    private void executeOauth(){
+    	//Twitetr4jの設定を読み込む
+    	Configuration conf = ConfigurationContext.getInstance();
+
+    	//Oauth認証オブジェクト作成
+		OAuthAuthorization oauth = new OAuthAuthorization(conf);
+		//Oauth認証オブジェクトにconsumerKeyとconsumerSecretを設定
+		oauth.setOAuthConsumer("iy2FEHXmSXNReJ6nYQ8FRg", "KYro4jM8BHlLSMsSdTylnTcm3pYaTCiG2UZrYK1yI4");
+		//アプリの認証オブジェクト作成
+		RequestToken _req = null;
+		try {
+			_req = oauth.getOAuthRequestToken("Callback://CallBackActivity");
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		String _uri;
+		_uri = _req.getAuthorizationURL();
+		startActivityForResult(new Intent(Intent.ACTION_VIEW , Uri.parse(_uri)), 0);
+    }
+}
