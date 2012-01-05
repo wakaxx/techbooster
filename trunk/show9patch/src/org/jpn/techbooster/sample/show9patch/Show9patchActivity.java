7975c624d8c96b0268de23b4813589a1d814d863
rabbitlog <rabbitlog@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-05 13:45:00 +0000

    "https://techbooster.googlecode.com/svn" にプロジェクト "show9patch" を共用

---
+++ trunk/show9patch/src/org/jpn/techbooster/sample/show9patch/Show9patchActivity.java
@@ -0,0 +1,13 @@
+package org.jpn.techbooster.sample.show9patch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Show9patchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
