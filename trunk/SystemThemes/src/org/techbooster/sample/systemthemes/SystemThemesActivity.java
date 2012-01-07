b1fad583a4ca32ba44ea964848f7e4afae24e896
rabbitlog@gmail.com <rabbitlog@gmail.com@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-07 05:16:22 +0000

    "https://techbooster.googlecode.com/svn" にプロジェクト "SystemThemes" を共用

---
+++ trunk/SystemThemes/src/org/techbooster/sample/systemthemes/SystemThemesActivity.java
@@ -0,0 +1,13 @@
+package org.techbooster.sample.systemthemes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SystemThemesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
