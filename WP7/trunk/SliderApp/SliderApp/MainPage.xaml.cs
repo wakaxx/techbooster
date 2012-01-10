41f230b3c8925233d9ba16eb89440dd419feeba7
muchiki0226 <muchiki0226@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-10 16:54:49 +0000

    スライダーの値を取得する

---
+++ WP7/trunk/SliderApp/SliderApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace SliderApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+            textBlock1.Text = "value: " + slider1.Value.ToString("f1");
+        }
+
+        private void SliderValueChanged(object sender,
+                RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                Slider s = (Slider)sender;
+                textBlock1.Text = "value: " + s.Value.ToString("f1");  
+            }
+            catch (NullReferenceException ex) { }
+        }
+    }
+}
