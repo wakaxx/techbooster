394d9fb2008db2cae4b9f8333e3a65ddccd39607
muchiki0226 <muchiki0226@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-10 16:56:25 +0000

    タッチイベントを取得する

---
+++ WP7/trunk/TouchApp/TouchApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace TouchApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+            Touch.FrameReported += new TouchFrameEventHandler(TouchFrameReported);
+        }
+
+        private void TouchFrameReported(object sender, TouchFrameEventArgs e)
+        {
+            TouchPoint touchPoint = e.GetPrimaryTouchPoint(null);
+
+            switch (touchPoint.Action)
+            {
+                case TouchAction.Down:
+                    textBlock1.Text = "Down \nX:" + touchPoint.Position.X + "\tY:" + touchPoint.Position.Y;
+                    break;
+                case TouchAction.Move:
+                    textBlock1.Text = "Move \nX:" + touchPoint.Position.X + "\tY:" + touchPoint.Position.Y;
+                    break;
+                case TouchAction.Up:
+                    textBlock1.Text = "Up \nX:" + touchPoint.Position.X + "\tY:" + touchPoint.Position.Y;
+                    break;
+            }
+
+            
+        }
+    }
+}
