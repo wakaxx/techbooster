d49285c486cd0b30c7d35168b2abc367f8238d70
muchiki0226 <muchiki0226@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-10 16:54:00 +0000

    画像を表示する

---
+++ WP7/trunk/ImageApp/ImageApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// アセンブリに関する一般情報は、以下の属性セットによって制御されます。 
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("ImageApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageApp")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから 
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、 
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("f29fc650-44eb-41cf-b0a0-c292533e0847")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次のように '*' を使ってリビジョンおよびビルド番号を 
+// 既定値にすることができます。
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
