f194ba1338975e5faccbac0fa41656e1b06f85c7
ragecolor.5884@gmail.com <ragecolor.5884@gmail.com@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-14 14:14:02 +0000

    first commit for ListBoxSample

---
+++ WP7/trunk/ListBoxSample/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace ListBoxSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+            /*
+            for (int cnt = 0; cnt < 20; cnt++)
+            {
+                listBox1.Items.Add("sample " + cnt);
+            }
+            */
+
+            initListParam();
+        
+        }
+
+        private void initListParam()
+        {
+            ListParamData kobashinA = new ListParamData("kobashin","000000000","kobashinA",1);
+            ListParamData kobashinB = new ListParamData("kobashin", "000000000", "kobashinB", 2);
+            ListParamData kobashinC = new ListParamData("kobashin", "000000000", "kobashinC", 3);
+            ListParamData kobashinD = new ListParamData("kobashin", "000000000", "kobashinD", 4);
+            ListParamData kobashinE = new ListParamData("kobashin", "000000000", "kobashinD", 5);
+
+            listBox1.Items.Add(kobashinA);
+            listBox1.Items.Add(kobashinB);
+            listBox1.Items.Add(kobashinC);
+            listBox1.Items.Add(kobashinD);
+            listBox1.Items.Add(kobashinE);
+
+
+
+        }
+
+    }
+
+    public class ListParamData
+    {
+        public String NAME;       // 名前
+        public String PHONE;     // 電話番号
+        public String TWITTER;   // TwitterId
+        public long _ID;              // ID
+
+        // コンストラクタ
+        public ListParamData(String mName, String mPhone, String mTwitter, long Id)
+        {
+            NAME = mName;
+            PHONE = mPhone;
+            TWITTER = mTwitter;
+            _ID = Id;
+        }
+
+        // 名前とTwitterIdの出力
+        public String dump()
+        {
+            return "name:" + NAME + "(@" + TWITTER + ")";
+        }
+
+        // ToStringメソッド
+        public override string ToString()
+        {
+            // ToStringメソッドでreturnした値がリスト表示される
+            return "" + _ID + " " + NAME;
+        }
+    }
+}
