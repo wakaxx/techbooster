a23634261e06254ae3051bf3576b3ddefdaad668
uent192837465@gmail.com <uent192837465@gmail.com@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-01-17 16:50:09 +0000

    ソースコード中身コミット

---
+++ trunk/LayoutoptSample/src/org/japan/techbooster/sample/layoutsample/LayoutSampleActivity.java
@@ -0,0 +1,14 @@
+package org.japan.techbooster.sample.layoutsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_layout);
+    }
+}
