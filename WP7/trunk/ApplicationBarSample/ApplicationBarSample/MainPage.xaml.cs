143deb65446ae1108ea820ddddf05abdd1193a27
ragecolor.5884@gmail.com <ragecolor.5884@gmail.com@9c01397b-c2b8-10e3-aeff-0333ccf75592>
2012-02-01 12:59:55 +0000

    ApplicationBarを使ってMenuを表示する(WP7)のサンプルプロジェクト

---
+++ WP7/trunk/ApplicationBarSample/ApplicationBarSample/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ApplicationBarSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_button_Click(object sender, EventArgs e)
+        {
+            textBlock1.Text = "Ok button clicked";
+        }
+
+        private void Cancel_button_Click(object sender, EventArgs e)
+        {
+            textBlock1.Text = "Cancel button clicked";
+        }
+
+
+        private void ApplicationBarMenuItem_Click_1(object sender, EventArgs e)
+        {
+            textBlock1.Text = "MenuItem1 Clicked";
+
+        }
+
+        private void ApplicationBarMenuItem_Click_2(object sender, EventArgs e)
+        {
+            textBlock1.Text = "MenuItem2 Clicked";
+
+        }
+
+        private void checkBox1_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox check = (CheckBox)sender;
+            ApplicationBarIconButton appBarIcon = this.ApplicationBar.Buttons[0] as ApplicationBarIconButton;
+
+            if ((bool)check.IsChecked)
+            {
+                appBarIcon.IsEnabled = false;
+            }
+            else
+            {
+                appBarIcon.IsEnabled = true;
+            }
+
+        }
+    }
+}
