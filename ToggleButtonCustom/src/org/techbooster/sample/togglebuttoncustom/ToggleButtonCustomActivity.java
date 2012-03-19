11d9b9e4ee8ad0469b9dfd28c0cf54aa5b544075
crossbridge.kato <crossbridge.kato@gmail.com>
2012-03-19 07:05:09 +0000

    初期インポート。

---
+++ ToggleButtonCustom/src/org/techbooster/sample/togglebuttoncustom/ToggleButtonCustomActivity.java
@@ -0,0 +1,13 @@
+package org.techbooster.sample.togglebuttoncustom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToggleButtonCustomActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
